447b8ee2431abad1b9859057f89fa84fa5d2b6a5
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-12-12 15:37:24 +0000

    marble: parts: Implement profile-specific Dolby settings Some refactoring and cleanup while we're at it.
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>
    Signed-off-by: AnilShebin <anilshebin@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -25,12 +25,14 @@ import org.lineageos.settings.thermal.ThermalUtils;
 public class BootCompletedReceiver extends BroadcastReceiver {
 
     private static final boolean DEBUG = false;
-    private static final String TAG = "XiaomiParts";
+    private static final String TAG = "XiaomiParts-BCR";
 
     @Override
     public void onReceive(final Context context, Intent intent) {
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
         ThermalUtils.startService(context);
+
+        Log.i(TAG, "Boot completed");
 
         // DisplayFeature
         ColorService.startService(context);
@@ -45,7 +47,7 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         AodBrightnessService.startService(context);
 
         // Dolby Atmos
-        DolbyUtils.getInstance(context).onBootCompleted();
+        DolbyUtils.getInstance(context);
 
         // Doze
         DozeUtils.checkDozeService(context);
