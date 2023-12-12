16a76ec6ec085c394fbf8f67d830a30a01f655a6
saikiran2001 <bjsaikiran@gmail.com>
2023-12-12 13:58:08 +0000

    marble: Bring dolby back from the dead
    Signed-off-by: AnilShebin <anilshebin@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -46,17 +46,17 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         AodBrightnessService.startService(context);
 
         // Dirac
-        try {
-            DiracUtils.getInstance(context);
-        } catch (Exception e) {
-            Log.d(TAG, "Dirac is not present in system");
-        }
+        // try {
+        //     DiracUtils.getInstance(context);
+        // } catch (Exception e) {
+        //     Log.d(TAG, "Dirac is not present in system");
+        // }
 
         // Dolby Atmos
-        // DolbyUtils.getInstance(context).onBootCompleted();
+        DolbyUtils.getInstance(context).onBootCompleted();
 
         // Doze
-        // DozeUtils.checkDozeService(context);
+        DozeUtils.checkDozeService(context);
 
         // Refresh Rate
         RefreshUtils.initialize(context);
